a006e4b1a5d04b479f2f554375eb0acc80c56121
Harinder Singh <admin@ezyhow.com>
2025-09-23 20:48:15 +0530

    15_Text_to_SQL_using_Prompt_Engineering

---
+++ 15_Text_to_SQL_using_Prompt_Engineering/departments.sql
@@ -0,0 +1,28 @@
+create table departments (
+	id INT,
+	department_name VARCHAR(50),
+	department_code VARCHAR(9),
+	department_location VARCHAR(50),
+	department_phone VARCHAR(50),
+	department_email VARCHAR(50)
+);
+insert into departments (id, department_name, department_code, department_location, department_phone, department_email) values (1, 'Legal', 'Sales', '9772 Norway Maple Plaza', '475-670-4661', 'nmcphaden0@quantcast.com');
+insert into departments (id, department_name, department_code, department_location, department_phone, department_email) values (2, 'Sales', 'HR', '30 Spaight Pass', '338-703-8902', 'elorand1@bizjournals.com');
+insert into departments (id, department_name, department_code, department_location, department_phone, department_email) values (3, 'Support', 'Marketing', '07 Pleasure Place', '918-175-9341', 'dcroose2@sitemeter.com');
+insert into departments (id, department_name, department_code, department_location, department_phone, department_email) values (4, 'Product Management', 'Marketing', '14 Kensington Circle', '661-407-8307', 'falexandersson3@guardian.co.uk');
+insert into departments (id, department_name, department_code, department_location, department_phone, department_email) values (5, 'Legal', 'Sales', '17 Crescent Oaks Place', '270-848-8878', 'omerwood4@hatena.ne.jp');
+insert into departments (id, department_name, department_code, department_location, department_phone, department_email) values (6, 'Research and Development', 'HR', '321 Nova Avenue', '682-468-9680', 'bghiroldi5@un.org');
+insert into departments (id, department_name, department_code, department_location, department_phone, department_email) values (7, 'Services', 'Marketing', '38997 Barnett Court', '998-193-4748', 'jwinsome6@vk.com');
+insert into departments (id, department_name, department_code, department_location, department_phone, department_email) values (8, 'Sales', 'Finance', '8572 Merry Trail', '998-332-3187', 'ogrimmert7@youku.com');
+insert into departments (id, department_name, department_code, department_location, department_phone, department_email) values (9, 'Accounting', 'Finance', '95543 Warner Hill', '152-933-2834', 'ffeathers8@boston.com');
+insert into departments (id, department_name, department_code, department_location, department_phone, department_email) values (10, 'Product Management', 'IT', '79042 Heffernan Alley', '912-354-1476', 'fbolus9@yale.edu');
+insert into departments (id, department_name, department_code, department_location, department_phone, department_email) values (11, 'Sales', 'HR', '8 Sachtjen Junction', '702-777-5046', 'kbreissana@godaddy.com');
+insert into departments (id, department_name, department_code, department_location, department_phone, department_email) values (12, 'Support', 'Finance', '49 Valley Edge Way', '201-865-1607', 'sattrillb@histats.com');
+insert into departments (id, department_name, department_code, department_location, department_phone, department_email) values (13, 'Accounting', 'IT', '4886 Main Terrace', '155-804-4908', 'srutterc@apache.org');
+insert into departments (id, department_name, department_code, department_location, department_phone, department_email) values (14, 'Engineering', 'IT', '30551 Paget Place', '541-293-6962', 'tkorousd@constantcontact.com');
+insert into departments (id, department_name, department_code, department_location, department_phone, department_email) values (15, 'Training', 'Finance', '302 Kim Drive', '742-135-3247', 'kbestwalle@ucoz.com');
+insert into departments (id, department_name, department_code, department_location, department_phone, department_email) values (16, 'Legal', 'Finance', '870 Lakewood Gardens Trail', '228-678-8681', 'esmallpeicef@imageshack.us');
+insert into departments (id, department_name, department_code, department_location, department_phone, department_email) values (17, 'Research and Development', 'IT', '0249 David Drive', '648-859-5029', 'droddyg@timesonline.co.uk');
+insert into departments (id, department_name, department_code, department_location, department_phone, department_email) values (18, 'Business Development', 'Sales', '3 Dakota Hill', '726-449-8705', 'gghilksh@ft.com');
+insert into departments (id, department_name, department_code, department_location, department_phone, department_email) values (19, 'Legal', 'IT', '5 Packers Crossing', '686-700-8157', 'ndalmoni@wsj.com');
+insert into departments (id, department_name, department_code, department_location, department_phone, department_email) values (20, 'Business Development', 'Marketing', '04861 Butternut Avenue', '730-966-0792', 'ndykej@indiatimes.com');
---
+++ 15_Text_to_SQL_using_Prompt_Engineering/designations.sql
@@ -0,0 +1,14 @@
+create table designations (
+	id INT,
+	designation_name VARCHAR(50)
+);
+insert into designations (id, designation_name) values (1, 'Cost Accountant');
+insert into designations (id, designation_name) values (2, 'Community Outreach Specialist');
+insert into designations (id, designation_name) values (3, 'Web Designer III');
+insert into designations (id, designation_name) values (4, 'Software Engineer III');
+insert into designations (id, designation_name) values (5, 'Graphic Designer');
+insert into designations (id, designation_name) values (6, 'Engineer IV');
+insert into designations (id, designation_name) values (7, 'Statistician III');
+insert into designations (id, designation_name) values (8, 'Programmer Analyst I');
+insert into designations (id, designation_name) values (9, 'Administrative Assistant I');
+insert into designations (id, designation_name) values (10, 'Sales Representative');
